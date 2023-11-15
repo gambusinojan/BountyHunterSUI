1f285e3cc0832e840c55ea0e6cdb006120ee1919
jan-zelaznog <jan.zelaznog@gmail.com>
2023-11-14 23:39:08 -0600

    Initial Commit

---
+++ BountyHunterSUI/BountyHunterSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BountyHunterSUIApp.swift
+//  BountyHunterSUI
+//
+//  Created by Ángel González on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BountyHunterSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
